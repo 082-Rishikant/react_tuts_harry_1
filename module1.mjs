ea7d93c6d1950c810155c9ec492c746323ef4014
Rishikant Patel <rishikant9650@gmail.com>
2021-12-04 20:34:43 +0530

    lecture 6 Props and PropTypes

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+let a="Rishi";
+const b="Rahul"
+let c="Rai"
+let d="Nikil";
+
+export default d;
+// export default c;
+
+export {a, b}
---
+++ module2.mjs
@@ -0,0 +1,5 @@
+import dc, {a, b} from './module1.mjs'
+
+console.log(dc)
+console.log(b)
+console.log(a)
