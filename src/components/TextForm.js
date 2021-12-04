b3d138920d5f583e18c66130e0f3537b5be4a8df
Rishikant Patel <rishikant9650@gmail.com>
2021-12-04 23:38:21 +0530

    Lecture-8 Adding more logic to TextUtils App

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,24 +1,36 @@
 import { useState } from "react"
-export function TextForm(props)
-{
-  const[text, setText]=useState('')
+export function TextForm(props) {
+  const [text, setText] = useState('')
   // text='new Text'; // wrong way to update state
   // setText('new Text') // right way to update state
-  function upperClick()
-  {
-    let newText=text.toUpperCase()
+  function upperClick() {
+    let newText = text.toUpperCase()
     setText(newText)
   }
-  const handleonChange=(event)=>{
+  const lowerClick = () => {
+    let newText = text.toLowerCase()
+    setText(newText)
+  }
+  const handleonChange = (event) => {
     setText(event.target.value)
   }
-  return(
+  return (
     <>
-    <h2>{props.heading}</h2>
-    <div className="mb-3">
-      <textarea className="form-control" value={text} onChange={handleonChange} id="myBox" rows="9" placeholder="Enter Text here"></textarea>
-    </div>
-    <button className="btn btn-primary" onClick={upperClick}>Convert to Uppercase</button>
+      <div className="container my-3">
+        <h2>{props.heading}</h2>
+        <div className="mb-3">
+          <textarea className="form-control" value={text} onChange={handleonChange} id="myBox" rows="9" placeholder="Enter Text here"></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={upperClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={lowerClick}>Convert to Uppercase</button>
+      </div>
+      <div className="container my-3">
+        <h3>Text Details</h3>
+        <p>{text.split(' ').length} words  {text.length} characters</p>
+        <p>{0.008*text.split(' ').length} minutes read time</p>
+        <h3>Text Preview</h3>
+        <p>{text}</p>
+      </div>
     </>
   )
 }
